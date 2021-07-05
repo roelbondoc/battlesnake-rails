10db6117822d3c0ced51679eb44df4a794bb5cf1
Roel Bondoc <rsbondoc@gmail.com>
2021-07-05 00:23:36 -0400

    Revert "Logging"
    This reverts commit e5d4a3a64c81cb80a4362771178eae6ec94b596e.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -50,8 +50,7 @@ Rails.application.configure do
 
   # Include generic and useful information about system operation, but avoid logging too much
   # information to avoid inadvertent exposure of personally identifiable information (PII).
-  # config.log_level = :info
-  config.active_record.verbose_query_logs = true
+  config.log_level = :info
 
   # Prepend all log lines with the following tags.
   config.log_tags = [ :request_id ]
