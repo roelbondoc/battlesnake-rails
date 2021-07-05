6b84181d9818c6dbf3e2feaa9d31507609e0ba13
Roel Bondoc <rsbondoc@gmail.com>
2021-07-05 16:03:32 -0400

    Trying to avoid traps

--- app/models/calculator.rb
+++ app/models/calculator.rb
@@ -7,6 +7,9 @@ class Calculator
   end
 
   def move
+    ((prioritized_moves & valid_moves) - (possible_hazard_moves + too_crowded_moves(1))).first ||
+    ((prioritized_moves & valid_moves) - (possible_hazard_moves + too_crowded_moves(2))).first ||
+    ((prioritized_moves & valid_moves) - too_crowded_moves).first ||
     ((prioritized_moves & valid_moves) - possible_hazard_moves).first ||
       (prioritized_moves & valid_moves).first ||
       (valid_moves - possible_hazard_moves).sample ||
@@ -34,36 +37,60 @@ class Calculator
 
   def valid_moves
     @valid_moves ||= [].tap do |moves|
-      moves << 'up' if can_move_up?
-      moves << 'down' if can_move_down?
-      moves << 'left' if can_move_left?
-      moves << 'right' if can_move_right?
+      moves << 'up' if can_move_up?(head_x, head_y)
+      moves << 'down' if can_move_down?(head_x, head_y)
+      moves << 'left' if can_move_left?(head_x, head_y)
+      moves << 'right' if can_move_right?(head_x, head_y)
     end
+  end
+
+  def number_of_valid_moves(x, y)
+    count = 0
+    count = count + 1 if can_move_up?(x, y)
+    count = count + 1 if can_move_down?(x, y)
+    count = count + 1 if can_move_left?(x, y)
+    count = count + 1 if can_move_right?(x, y)
+    count
+  end
+
+  def next_moves_number_of_moves
+    @next_moves_number_of_moves ||= [].tap do |moves|
+      moves << ['up', number_of_valid_moves(head_x, head_y+1)] if valid_moves.include?('up')
+      moves << ['down', number_of_valid_moves(head_x, head_y-1)] if valid_moves.include?('down')
+      moves << ['left', number_of_valid_moves(head_x-1, head_y)] if valid_moves.include?('left')
+      moves << ['right', number_of_valid_moves(head_x+1, head_y)] if valid_moves.include?('right')
+    end
+  end
+
+  def too_crowded_moves(moves_to_filter_out)
+    next_moves_number_of_moves.map do |dir, number_of_moves|
+      dir if number_of_moves < moves_to_filter_out
+    end.compact
   end
 
   def possible_hazard_moves
     @possible_hazard_moves ||= [].tap do |moves|
       moves << 'up' if possible_stonger_head?(head_x, head_y+1)
       moves << 'down' if possible_stonger_head?(head_x, head_y-1)
-      moves << 'left' if possible_stonger_head?(head_x+1, head_y)
-      moves << 'right' if possible_stonger_head?(head_x-1, head_y)
+      moves << 'left' if possible_stonger_head?(head_x-1, head_y)
+      moves << 'right' if possible_stonger_head?(head_x+1, head_y)
     end
   end
 
-  def can_move_up?
-    head_y < max_y-1 && empty?(head_x, head_y+1)
+  def can_move_up?(x, y)
+    y < max_y-1 && empty?(x, y+1)
   end
 
-  def can_move_down?
-    head_y > 0 && empty?(head_x, head_y-1)
+  def can_move_down?(x, y)
+    y > 0 && empty?(x, y-1)
   end
 
-  def can_move_left?
-    head_x > 0 && empty?(head_x-1, head_y)
+  def can_move_left?(x, y)
+    x > 0 && empty?(x-1, y)
   end
 
-  def can_move_right?
-    head_x < max_x-1 && empty?(head_x+1, head_y)
+  def can_move_right?(x, y)
+    x < max_x-1 && empty?(x+1, y)
   end
 
   def health_in_danger?
